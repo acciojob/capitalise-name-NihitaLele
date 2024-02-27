a240a7398127f3cbb1d5a8b4fbf19fb4e91e909a
Nihita Lele <124602601+NihitaLele@users.noreply.github.com>
2024-02-27 23:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+document.getElementById('fname').addEventListener('blur', function() {
+  var nameInput = document.getElementById('fname');
+  var name = nameInput.value;
+
+  var uppercaseName = name.toUpperCase();
+
+  nameInput.value = uppercaseName;
+});
